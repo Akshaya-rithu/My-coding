cab2f95dc88f14204a436201c1861e74aaea222a
Akshaya-rithu <akshaya02rithu@gmail.com>
2020-05-26 00:32:32 +0530

    Rithu added coding

--- src/org/hcl/test/Base.java
+++ src/org/hcl/test/Base.java
@@ -3,6 +3,8 @@ package org.hcl.test;
 public class Base extends Sample{
 	public  void parentmethod() {
 		System.out.println("Parent method");
+		System.out.println("Child method 1");
+		System.out.println("Child method 2");
 	}
 	public static void main(String[] args) {
 		Sample b=new Base();
