7eb69c8c3e22fd9eee5e6e9ccb3f6712d51aaf32
Akshaya-rithu <akshaya02rithu@gmail.com>
2020-05-26 00:13:59 +0530

    Akshaya added lines in her coding

--- src/org/hcl/test/Base.java
+++ src/org/hcl/test/Base.java
@@ -3,6 +3,8 @@ package org.hcl.test;
 public class Base extends Sample{
 	public  void parentmethod() {
 		System.out.println("Parent method");
+		System.out.println("Parent method 2");
+		System.out.println("Parent method 3");
 	}
 	public static void main(String[] args) {
 		Sample b=new Base();
